f5c75168724811010da5c10fd0f99b1960bf3fec
Enrique Gomez <enrique.gomez.electronico@gmail.com>
2019-11-13 20:04:52 +0100

    Redirect index page

--- API/public/cms.js
+++ API/public/cms.js
@@ -23,7 +23,7 @@
     var latestRelease = JSON.parse(this.responseText);
     console.log(latestRelease);
     if (actual != latestRelease.tag_name){
-      document.getElementById('HomeWareStatus').innerHTML += '<p style="background-color:#81F79F; padding:20px;"> <b>New versión available:</b> ' + latestRelease.tag_name + ' <br> <b>Description:</b> ' + latestRelease.body + ' <br> <b>Download</b> it from <a href="https://github.com/kikeelectronico/Homeware/releases/tag/v0.6.1" target="blanck">here</a> </p> ';
+      document.getElementById('HomeWareStatus').innerHTML += '<p style="background-color:#81F79F; padding:20px;"> <b>New version available:</b> ' + latestRelease.tag_name + ' <br> <b>Description:</b> ' + latestRelease.body + ' <br> <b>Download</b> it from <a href="https://github.com/kikeelectronico/Homeware/releases/tag/v0.6.1" target="blanck">here</a> </p> ';
     } else {
       document.getElementById('HomeWareStatus').innerHTML += 'Your system is up to date';
     }
