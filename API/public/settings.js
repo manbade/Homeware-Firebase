91fe4a3625ee4f20bb66aa3689ccf41074e5a4b2
Enrique Gomez <enrique.gomez.electronico@gmail.com>
2019-11-15 20:41:16 +0100

    Key for authorization code implemented & ddbb example updated.

--- API/public/settings.js
+++ API/public/settings.js
@@ -10,17 +10,42 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
 });
 
 // Get a statusRef from the database service
-var settingsRef = firebase.database().ref().child('settings');
+var database = firebase.database();
 
-//Read the autoAuthentication status
+//Auto save the autoAuthentication status
 autoAuthentication.addEventListener('change', function() {
   //Update the text message
   document.getElementById('textMessageAlert').innerHTML = '...';
+  //Get the status switch
   var status = document.getElementById('autoAuthentication').checked;
-  settingsRef.update({autoAuthentication: status})
+  //Save the status
+  database.ref('/settings/bools/').update({autoAuthentication: status})
     .then(function(){
-      //update the text message
-      document.getElementById('textMessageAlert').innerHTML = 'Saved';
+      //Get the settings
+      return database.ref('/settings/strings/').once('value');
+    })
+    .then(function(settingsSnapshot){
+      //Change the authorization code at the database
+      codeKey = settingsSnapshot.val().codeKey;
+      return database.ref('/token/').once('value');
+    })
+    .then(function(tokenSnapshot){
+      var ids = Object.keys(tokenSnapshot.val());
+      ids.forEach(function(id){
+        if(id != 'google'){
+          if (status) {
+            authorizationCode = id + codeKey;
+          } else {
+            //Generate a random value
+            authorizationCode = Math.random()*1000;
+            //authorizationCode = '-';
+          }
+          database.ref('/token/').child(id).child('authorization_code').update({ value: authorizationCode});
+        }
+      });
+
+      //Update the text message
+      updateMessageWithTime();
     })
     .catch(function(error){
       //update the text message and log the error
@@ -29,12 +54,33 @@ autoAuthentication.addEventListener('change', function() {
     });
 });
 
-//Load values
-settingsRef.once('value')
+//Manual save
+save.addEventListener('click', function() {
+  //Update the text message
+  document.getElementById('textMessageAlert').innerHTML = '...';
+  //Save the data
+  database.ref('/settings/strings/').update({
+    codeKey: document.getElementById('codeKey').value
+  })
+  //Update the text message
+  updateMessageWithTime();
+});
+
+//Load bools values
+database.ref('/settings/bools/').once('value')
   .then(function(dataSnapshot){
     var settings = dataSnapshot.val();
     Object.keys(settings).forEach(function(setting){
       document.getElementById(setting).checked = settings[setting];
+    });
+  })
+
+//load string values
+database.ref('/settings/strings/').once('value')
+  .then(function(dataSnapshot){
+    var settings = dataSnapshot.val();
+    Object.keys(settings).forEach(function(setting){
+      document.getElementById(setting).value = settings[setting];
     });
   })
 
@@ -46,13 +92,29 @@ function updateModal(settings){
   if (settings == 'autoAuthentication'){
     title = 'Automatic authentication';
     paragraph = 'If <b>False</b> manual authentication must be done from the <i>Device</i> section:<br>  <ul><li>For new devices</li> <li>If the device lost its refresh token</li></ul> If <b>True</b> the devices will autoconnect using its <i>Authorization code</i>.';
-  } else if (settings == 'authorizationCodeStructure'){
-    title = 'Authorization code structure';
-    paragraph = 'This is the structure that the API will use to auto generate authorization codes.';
+  } else if (settings == 'codeKey'){
+    title = 'Key for authorization code';
+    paragraph = 'The authorization code is compose with the device\'s id and the Key for authorization code. <br><br> For example, if the device\'s id is <b><i>"bedroom-light"</i></b> and the key is <b><i>"-code"</i></b> the athorization code will be <b><i>"bedroom-light-code"</i></b>.';
   }
 
 
   document.getElementById('learMoreModalTitle').innerHTML = title;
   document.getElementById('learMoreModalParagraph').innerHTML = paragraph;
 
+}
+
+//Print message after save
+function addZero(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+function updateMessageWithTime(){
+  var d = new Date();
+  var h = addZero(d.getHours());
+  var m = addZero(d.getMinutes());
+  var s = addZero(d.getSeconds());
+  document.getElementById('textMessageAlert').innerHTML = 'Saved at ' + h + ":" + m + ":" + s;
 }
