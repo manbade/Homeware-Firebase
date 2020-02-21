b15b2232bf994521a8869884f80f66c9678bb5d2
Manbade VIA Manbait Untamed REEF <cicloide64@gmail.com>
2020-02-21 09:27:02 +0100

    Update thermostat.ino
    Some changes to other kind of sensor and reading and writing values

--- Arduino Library for ESP8266/examples/thermostat/thermostat.ino
+++ Arduino Library for ESP8266/examples/thermostat/thermostat.ino
@@ -2,8 +2,13 @@
 #include <ArduinoJson.h>
 #include <EEPROM.h>
 #include <Wire.h>
-#include <Adafruit_BMP280.h>
 
+#include <DHT.h>
+#define DHTTYPE DHT22
+#define DHTPIN  D1
+DHT dht(DHTPIN, DHTTYPE, 22); // for ESP8266 use dht(DHTPIN, DHTTYPE, 11)
+float temperature = 21;
+float humidity = 80;
 
 //Config this section
 const char* ssid = "your-wifi-ssid";
@@ -14,13 +19,19 @@ char* id = "thermostat";
 //General global variables
 long int time_value = 0;
 int outputEEPROM = 10;
-char json_c[200];
+char json_c[300];
 char mode[30];
 int temperatureSetPoint = 0;
 
+float thermostatHumidityAmbient = 0;
+float thermostatTemperatureAmbient = 0;
+float thermostatTemperatureSetpoint = 0;
+float thermostatTemperatureSetpointHigh = 0;
+float thermostatTemperatureSetpointLow = 0;
+
 //Objects
 WiFiClientSecure client;
-Adafruit_BMP280 bmp;
+DHT dht(DHTPIN, DHTTYPE, 22); // for ESP8266 use dht(DHTPIN, DHTTYPE, 11)
 Homeware api(id, host, &client);
 
 void setup() {
@@ -49,9 +60,8 @@ void setup() {
   Serial.println(F("Getting token"));
   api.getToken(false);
 
-  if (!bmp.begin()) {
-    Serial.println(F("Could not find the sensor, check wiring or I2C address"));
-  }
+  dht.begin();
+
 }
 
 void loop() {
@@ -68,26 +78,52 @@ void loop() {
   }
 
   //Get state or token
-  if (millis() - time_value > 1000){
+  if (millis() - time_value > 2000){  
+    // this code sends and read each 2 seconds to firebase.
+    // This device can send the temperature 1 minute or longer and only send the preset when change.
     //Send temperature
-    char n[2];
-    int i=bmp.readTemperature();
-    sprintf(n, "%d", i);
-    api.sendTrait("thermostatTemperatureAmbient", n, "int");
-    //Get mode and temperarure
+    temperature = dht.readTemperature(false);
+    char temp_char[5];
+    dtostrf(temperature, 2, 2, temp_char);
+    api.sendTrait("thermostatTemperatureAmbient", temp_char, "char");
+    humidity = dht.readHumidity();
+    char hum_char[5];
+    dtostrf(humidity, 2, 2, hum_char);
+    api.sendTrait("thermostatHumidityAmbient", hum_char, "char");
+    /*
+    . Send code for change temperature limit values
+    . when press up or down setting temperature buttons in the device i.e.
+    .
+    .
+    */
+
+    
+    //Get mode and temperature
     strcpy(json_c, api.getJSON());
-    StaticJsonDocument<200> doc;
+    StaticJsonDocument<300> doc;
+    
+    // for testing mode and understand the jcon output
+    Serial.println(json_c);
+
     DeserializationError error = deserializeJson(doc, json_c);
     if (error) {
       Serial.print(F("deserializeJson() failed: "));
       Serial.println(error.c_str());
     } else {
-      strcpy(mode, doc["thermostatMode"]);
-      temperatureSetPoint = doc["thermostatTemperatureSetpoint"];
-      Serial.print(mode);
-      Serial.print(" - ");
-      Serial.println(temperatureSetPoint);
+      //strcpy(mode, doc["thermostatMode"]); // the json returns "online":true or "online":false but never "thermostatMode" this cause reset
+      //temperatureSetPoint = doc["thermostatTemperatureSetpoint"];
+      thermostatTemperatureAmbient = doc["thermostatTemperatureAmbient"];
+      thermostatHumidityAmbient = doc["thermostatHumidityAmbient"];
+      thermostatTemperatureSetpoint = doc["thermostatTemperatureSetpoint"];
+      // SetPoint high and Low are not interesting for me
+      // this could be useful when triggering a hot and cold HVAC unit i.e.
 
+      Serial.print("Humedad : ");
+      Serial.println(thermostatHumidityAmbient);
+      Serial.print("Temperatura : ");
+      Serial.println(thermostatTemperatureAmbient);
+      Serial.print("Preset : ");
+      Serial.println(thermostatTemperatureSetpoint);
     }
     time_value = millis();
   }
